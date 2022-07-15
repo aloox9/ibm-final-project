5a71c870b505d5fc6284aa535c6084b331483a6c
aloox9 <75814424+aloox9@users.noreply.github.com>
2022-07-15 16:24:29 +0300

    Update simple_interest.sh

--- simple_interest.sh
+++ simple_interest.sh
@@ -2,7 +2,12 @@
    # This script calculates simple interest given principal,
    # annual rate of interest and time period in years.
 
+   # Do not use this in production. Sample purpose only.
+
    # Author: Upkar Lidder (IBM)
+   # Additional Authors:
+   # Ali Dirani
+
    # Input:
    # p, principal amount
    # t, time period in years
